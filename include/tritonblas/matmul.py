88242386dd6ecab51d56b48c56a59d438dd2336b
Xiaohu Guo <Xiaohu.Guo@amd.com>
2025-10-07 06:51:36 -0500

    update tritonblas matmul wraper for fp8/int8 per tensor scale

--- include/tritonblas/matmul.py
+++ include/tritonblas/matmul.py
@@ -4,7 +4,9 @@ import random
 import functools
 import time
 from .internal.persistent_matmul import persistent_matmul
+from .internal.persistent_matmul_a8w8 import persistent_matmul_a8w8
 from .internal.streamk_matmul import streamk_matmul
+from .internal.streamk_matmul_a8w8 import streamk_matmul_a8w8
 from .origami import MatmulHeuristicResult
 from typing import Dict, Tuple, Optional
 
@@ -105,6 +107,65 @@ def persistent_matmul_lt(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, sele
 
     return c
 
+def persistent_matmul_a8w8_lt(a: torch.Tensor, b: torch.Tensor, a_scale: torch.Tensor, b_scale: torch.Tensor, c: torch.Tensor, selector):
+    assert a.shape[1] == b.shape[0], "Incompatible Dimensions"
+    M, K = a.shape
+    _, N = b.shape
+
+    BLK_M, BLK_N, BLK_K, gsize_m = selector.get_config()
+
+    total_blocks_M = triton.cdiv(M, BLK_M)
+    total_blocks_N = triton.cdiv(N, BLK_N)
+    total_tiles = total_blocks_M * total_blocks_N
+    total_programs = total_tiles
+    even_k = K % BLK_K == 0
+
+    # TODO: Separate these configs.
+    # basica configs for most of compute bound sizes
+    # TODO: set these values analytically?
+    num_stages = 2
+    num_warps = 8
+    waves_per_eu = 0
+    mfmaInstrSize = 16
+    kpack = 1
+
+    # Run in Data-parallel mode.
+    grids = total_tiles
+
+    # TODO: Support other matmul algs.
+    kk = persistent_matmul_a8w8[(grids,)](
+        a,
+        b,
+        c,
+        a_scale,
+        b_scale,
+        None,  # TODO: Enable bias.
+        M,
+        N,
+        K,
+        a.stride(0),
+        b.stride(1),
+        c.stride(0),
+        c.stride(1),
+        0,  # TODO: Enable bias stride.
+        stride_ak=a.stride(1),
+        stride_bk=b.stride(0),
+        BLOCK_SIZE_M=BLK_M,
+        BLOCK_SIZE_N=BLK_N,
+        BLOCK_SIZE_K=BLK_K,
+        GROUP_SIZE_M=gsize_m,
+        NUM_SMS=total_programs,
+        NUM_XCDS=8,
+        BIAS=False,
+        EVEN_K=even_k,
+        num_stages=num_stages,
+        num_warps=num_warps,
+        waves_per_eu=waves_per_eu,
+        matrix_instr_nonkdim=mfmaInstrSize,
+        kpack=kpack,
+    )
+
+    return c
 
 def streamk_matmul_lt(
     a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, selector, sk_grid: Optional[int] = None
@@ -196,6 +257,91 @@ def streamk_matmul_lt(
 
     return c
 
+def streamk_matmul_a8_w8_lt(
+    a: torch.Tensor, b: torch.Tensor, a_scale: torch.Tensor, b_scale: torch.Tensor, c: torch.Tensor, selector, sk_grid: Optional[int] = None
+):
+    assert a.shape[1] == b.shape[0], "Incompatible Dimensions"
+    M, K = a.shape
+    _, N = b.shape
+
+    BLK_M, BLK_N, BLK_K, gsize_m = selector.get_config()
+
+    total_blocks_M = triton.cdiv(M, BLK_M)
+    total_blocks_N = triton.cdiv(N, BLK_N)
+    total_tiles = total_blocks_M * total_blocks_N
+    even_k = K % BLK_K == 0
+
+    ##
+    # Grid Size
+    ##
+    total_programs_streamk = selector.get_grid()
+
+    if total_programs_streamk > 0:  # Stream-K
+        total_tiles_streamk = total_tiles % total_programs_streamk
+    else:  # all tiles are computed using classical blocking
+        total_tiles_streamk = 0
+
+    num_stages = 2
+    num_warps = 8
+    waves_per_eu = 0
+    mfmaInstrSize = 16
+    kpack = 1
+    #for skinny size like 4, 5120, 2880, use CACHE_MODIFIER=".cg"
+    CACHE_MODIFIER_A = None
+    CACHE_MODIFIER_B = None
+
+    if sk_grid is not None:
+        total_programs_streamk = sk_grid
+
+    grids = total_programs_streamk
+    block_size = BLK_M * BLK_N
+
+    # Use global buffers with optimized zeroing
+    if grids <= MAX_SMS and block_size <= MAX_BLOCK_SIZE:
+        locks = _global_locks[:grids]
+        P = _global_P[:grids, :block_size]
+    else:
+        locks = torch.empty(grids, device="cuda", dtype=torch.uint8)
+        P = torch.empty(grids, block_size, device="cuda", dtype=torch.float32)
+
+    kk = streamk_matmul[(grids,)](
+        a,
+        b,
+        c,
+        a_scale,
+        b_scale,
+        None,  # TODO: Enable bias.
+        P,
+        locks,
+        M,
+        N,
+        K,
+        a.stride(0),
+        b.stride(1),
+        c.stride(0),
+        c.stride(1),
+        0,  # TODO: Enable bias stride.
+        stride_ak=a.stride(1),
+        stride_bk=b.stride(0),
+        BLOCK_SIZE_M=BLK_M,
+        BLOCK_SIZE_N=BLK_N,
+        BLOCK_SIZE_K=BLK_K,
+        GROUP_SIZE_M=gsize_m,
+        NUM_SMS=grids,
+        NUM_XCDS=8,
+        STREAMK_TILES=total_tiles_streamk,
+        BIAS=False,
+        EVEN_K=even_k,
+        CACHE_MODIFIER_A=CACHE_MODIFIER_A,
+        CACHE_MODIFIER_B=CACHE_MODIFIER_B,
+        num_stages=num_stages,
+        num_warps=num_warps,
+        waves_per_eu=waves_per_eu,
+        matrix_instr_nonkdim=mfmaInstrSize,
+        kpack=kpack,
+    )
+
+    return c
 
 def matmul_lt(
     a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, selector, enable_streamk=False
@@ -207,6 +353,15 @@ def matmul_lt(
     else:
         return persistent_matmul_lt(a, b, c, selector)
 
+def matmul_a8w8_lt(
+    a: torch.Tensor, b: torch.Tensor, a_scale: torch.Tensor, b_scale: torch.Tensor, c: torch.Tensor, selector, enable_streamk=False
+):
+    assert a.shape[1] == b.shape[0], "Incompatible Dimensions"
+
+    if enable_streamk:
+        return streamk_matmul_a8w8_lt(a, b, a_scale, b_scale, c, selector)
+    else:
+        return persistent_matmul_a8w8_lt(a, b, a_scale, b_scale, c, selector)
 
 def matmul(
     a: torch.Tensor,
@@ -225,72 +380,21 @@ def matmul(
     else:
         return persistent_matmul_lt(a, b, c, selector)
 
-def persistent_matmul_a8w8_lt(a: torch.Tensor, b: torch.Tensor, a_scale: torch.Tensor, b_scale: torch.Tensor, c: torch.Tensor, selector):
+def matmul_a8w8(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    a_scale: torch.Tensor,
+    b_scale: torch.Tensor,
+    c: torch.Tensor,
+    enable_streamk=False,
+    sk_grid=None,
+):
     assert a.shape[1] == b.shape[0], "Incompatible Dimensions"
     M, K = a.shape
     _, N = b.shape
 
-    BLK_M, BLK_N, BLK_K, gsize_m = selector.get_config()
-
-    total_blocks_M = triton.cdiv(M, BLK_M)
-    total_blocks_N = triton.cdiv(N, BLK_N)
-    total_tiles = total_blocks_M * total_blocks_N
-    total_programs = total_tiles
-    even_k = K % BLK_K == 0
-
-    # TODO: Separate these configs.
-    # basica configs for most of compute bound sizes
-    # TODO: set these values analytically?
-    num_stages = 2
-    num_warps = 8
-    waves_per_eu = 0
-    mfmaInstrSize = 16
-    kpack = 1
-
-    # Run in Data-parallel mode.
-    grids = total_tiles
-
-    # TODO: Support other matmul algs.
-    kk = persistent_matmul_a8w8[(grids,)](
-        a,
-        b,
-        c,
-        a_scale_ptr,
-        b_scale_ptr,
-        None,  # TODO: Enable bias.
-        M,
-        N,
-        K,
-        a.stride(0),
-        b.stride(1),
-        c.stride(0),
-        c.stride(1),
-        0,  # TODO: Enable bias stride.
-        stride_ak=a.stride(1),
-        stride_bk=b.stride(0),
-        BLOCK_SIZE_M=BLK_M,
-        BLOCK_SIZE_N=BLK_N,
-        BLOCK_SIZE_K=BLK_K,
-        GROUP_SIZE_M=gsize_m,
-        NUM_SMS=total_programs,
-        NUM_XCDS=8,
-        BIAS=False,
-        EVEN_K=even_k,
-        num_stages=num_stages,
-        num_warps=num_warps,
-        waves_per_eu=waves_per_eu,
-        matrix_instr_nonkdim=mfmaInstrSize,
-        kpack=kpack,
-    )
-
-    return c
-
-def smatmul_lt(
-    a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, selector, enable_streamk=False
-):
-    assert a.shape[1] == b.shape[0], "Incompatible Dimensions"
-
+    selector = _make_matmul_selector(M, N, K, a.dtype, b.dtype, c.dtype)
     if enable_streamk:
-        return streamk_matmul_lt(a, b, c, selector)
+        return streamk_matmul_a8w8_lt(a, b, a_scale, b_scale, c, selector, sk_grid=sk_grid)
     else:
-        return persistent_matmul_a8w8_lt(a, b, c, a_scale, b_scale, selector)
+        return persistent_matmul_a8w8_lt(a, b, a_scale, b_scale, c, selector)
